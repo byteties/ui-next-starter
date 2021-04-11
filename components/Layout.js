70f4b8b030151cce2239a038f148048c7137b2e6
Chayangkoon Dokhom <chayangkoondokhom@gmail.com>
2021-04-11 22:27:43 +0700

    update ui and style

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,15 @@
 import { Button,Layout,Card,Row,Col } from 'antd'
+import { CaretLeftOutlined,CaretRightOutlined } from '@ant-design/icons';
+import PandaIcon from '../public/panda'
 import styles from '../styles/components/layout.module.scss'
 import Header from '../components/Header'
-import Conversation from '../public/images/card-conversation-ai.svg'
-import Relevance from '../public/images/card-relevance-personalization.svg'
-import Retail from '../public/images/card-retail.svg'
-import Car from '../public/images/card-self-driving-cars.svg'
+// import Conversation from '../public/images/card-conversation-ai.svg'
+// import Relevance from '../public/images/card-relevance-personalization.svg'
+// import Retail from '../public/images/card-retail.svg'
+// import Car from '../public/images/card-self-driving-cars.svg'
+import Image from 'next/image'
 
 const { Content } = Layout;
-
 
 export default function LayoutMain({ children }) {
   return (
@@ -18,33 +20,100 @@ export default function LayoutMain({ children }) {
         <div className={styles.body}>
           <Row className={styles.section}>
             <Col span={12}>
-              <Row><h1>Welcome to Shop Management</h1></Row>
-              <Row>ร้านค้าขนมและเครื่องดื่ม</Row>
+              <Row><h1>Chayangkoon Dokhom</h1></Row>
+              <Row><h5>It' s my beginning website for me</h5></Row>
             </Col>
             <Col span={12}>
               <Row className={styles.row} justify="end" align="middle">
                 <Col>                
-                  <Button type="primary" shape="round" size="large">Get Started</Button>
+                  <Button className={styles.start} shape="round" size="large"><p>Get Started</p></Button>
                 </Col>
                 <Col offset={2}>
-                  <Button type="primary" shape="round" size="large">Talk To Us</Button>
+                  <Button shape="round" size="large">Talk To Us</Button>
                 </Col>
               </Row>
             </Col>
           </Row>
-          <Row className={styles.section}>
-            <Card className={styles.card}>
-              <Conversation className={styles.svg}/>
+          <Row className={styles.section} justify="space-around" align="middle">
+            <Col>
+              <Card className={styles.card}
+                cover={
+                <Image
+                  src="/images/profile.jpg"
+                  width={250} 
+                  height={306} 
+                />}
+                actions={[
+                  <CaretLeftOutlined />,
+                  <CaretRightOutlined />,
+                ]}
+              >
+                <Card.Meta
+                  avatar={<PandaIcon style={{ fontSize: '32px' }} />}
+                  title="Card title"
+                  description="This is the description"
+                />
+              </Card>
+            </Col>
+            <Col>
+              <Card className={styles.card}
+                cover={
+                <Image
+                  src="/images/profile.jpg"
+                  width={250} 
+                  height={306}   
+                />}
+                actions={[
+                  <CaretLeftOutlined />,
+                  <CaretRightOutlined />,
+                ]}
+              >
+                <Card.Meta
+                  avatar={<PandaIcon style={{ fontSize: '32px' }} />}
+                  title="Card title"
+                  description="This is the description"
+                />
+              </Card>
+            </Col>
+            <Col>
+              <Card className={styles.card}
+                cover={
+                <Image
+                  src="/images/profile.jpg"
+                  width={250} 
+                  height={306} 
+                />}
+                actions={[
+                  <CaretLeftOutlined />,
+                  <CaretRightOutlined />,
+                ]}
+              ><Card.Meta
+                avatar={<PandaIcon style={{ fontSize: '32px' }} />}
+                title="Card title"
+                description="This is the description"
+              />
             </Card>
-            <Card className={styles.card}>
-              <Relevance className={styles.svg}/>
-            </Card>
-            <Card className={styles.card}>
-              <Retail className={styles.svg}/>
-            </Card>
-            <Card className={styles.card}>
-              <Car className={styles.svg}/>
-            </Card>
+            </Col>
+            <Col>
+              <Card className={styles.card}
+                cover={
+                <Image
+                  src="/images/profile.jpg"
+                  width={250} 
+                  height={306}   
+                />}
+                actions={[
+                  <CaretLeftOutlined />,
+                  <CaretRightOutlined />,
+                ]}
+              >
+                <Card.Meta
+                  avatar={<PandaIcon style={{ fontSize: '32px' }} />}
+                  title="Card title"
+                  description="This is the description"
+                />
+              </Card>
+            </Col>
           </Row>
         </div>
       </Content>
